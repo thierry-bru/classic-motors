57fd7a79104fcfa89265bf4aab851283a0994e87
Thierry BRU <thierry-bru@laposte.net>
2025-04-24 11:53:37 +0200

    correction fichier deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 
 docker compose -f app/blog.networked-hells.com/docker-compose.yml build --no-cache
 docker compose -f app/blog.networked-hells.com/docker-compose.yml up --wait 
-docker compose -f shop/blog.networked-hells.com/docker-compose.yml build --no-cache
+docker compose -f app/shop.networked-hells.com/docker-compose.yml build --no-cache
 docker compose -f app/shop.networked-hells.com/docker-compose.yml up --wait 
