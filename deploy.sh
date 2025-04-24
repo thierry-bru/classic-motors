7e4cbc3c73ecaafebd13b96bcc7a0b4fca1e6384
Thierry BRU <thierry-bru@laposte.net>
2025-04-24 13:17:08 +0200

    déplacement des dockers compose et vidange

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-
-docker compose -f app/blog.networked-hells.com/docker-compose.yml build --no-cache
-docker compose -f app/blog.networked-hells.com/docker-compose.yml up --wait 
-docker compose -f app/shop.networked-hells.com/docker-compose.yml build --no-cache
-docker compose -f app/shop.networked-hells.com/docker-compose.yml up --wait 
