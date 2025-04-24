b15e1ed875b7051f9627e6b7bd67083e82c2befd
Thierry BRU <thierry-bru@laposte.net>
2025-04-24 10:22:53 +0200

    ajout deploy.sh pour déployer les applications

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+
+docker compose -f app/blog.networked-hells.com/docker-compose.yml build --no-cache
+docker compose -f app/blog.networked-hells.com/docker-compose.yml up --wait 
+docker compose -f shop/blog.networked-hells.com/docker-compose.yml build --no-cache
+docker compose -f app/shop.networked-hells.com/docker-compose.yml up --wait 
